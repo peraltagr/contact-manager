a8ea2510b3c7f544e5ee023bea933dc593afb2ec
peraltagr <peraltagr96@gmail.com>
2024-07-04 02:44:06 -0600

    port update react

--- frontend/src/ContactList.jsx
+++ frontend/src/ContactList.jsx
@@ -7,7 +7,7 @@ const ContactList = ({ contacts, updateContact, updateCallback }) => {
         method: "DELETE",
       };
       const response = await fetch(
-        `http://127.0.0.1:5000/delete_contact/${id}`,
+        `http://127.0.0.1:8000/delete_contact/${id}`,
         options
       );
       if (response.status === 200) {
